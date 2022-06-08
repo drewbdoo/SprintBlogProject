214592061e8f7a819bb62378bff7774ebeba80fb
Drew <drewbdoo@gmail.com>
2022-06-08 11:44:57 -0400

    init repo

---
+++ src/main/java/blog/CarBlogApplication.java
@@ -0,0 +1,13 @@
+package blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarBlogApplication.class, args);
+	}
+
+}
